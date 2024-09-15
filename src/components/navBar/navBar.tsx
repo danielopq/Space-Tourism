ab0865d85295d6c0c37947353cddde186a255dd9
danielopq <danielopq@gmail.com>
2024-09-15 16:53:11 +0100

    navBar - first steps

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,11 @@
+import './navBar.css';
+
+const NavBar = () =>{
+    return(
+        <nav>
+        <div id="logo"></div>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import NavBar from './components/navBar/navBar'
 import '../src/assets/css/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <NavBar/>
   </StrictMode>,
 )
