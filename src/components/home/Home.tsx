fcc8af999da83dbc3b7d29bdd8c792f8c7550225
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-19 15:36:10 +0100

    Home.tsx - text general layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,17 @@
 import './home.css'
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <main id="home">
             <div id="homeMainContent">
-
+                <div id="homeText">
+                    <p id="homeTextHeader">SO, YOU WANT TO TRAVEL TO</p>
+                    <h1>SPACE</h1>
+                    <p id="homeTextMain" className='mainText'>Let’s face it; if you want to go to space, you might as well genuinely go to
+                        outer space and not hover kind of on the edge of it. Well sit back, and relax
+                        because we’ll give you a truly out of this world experience!
+                    </p>
+                </div>
+                <div id="homeExploreBt"></div>
             </div>
         </main>
     )
