5b1d72dbd47cdb6f6a1bc046ccf7efcda0485a1d
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-19 13:43:58 +0100

    Technology.tsx - created

--- src/SpaceTourismApp.tsx
+++ src/SpaceTourismApp.tsx
@@ -3,6 +3,7 @@ import NavBar from './components/navBar/NavBar';
 import Home from "./components/home/Home";
 import Destination from "./components/destination/Destination";
 import Crew from "./components/crew/Crew";
+import Technology from "./components/technology/Technology";
 
 const SpaceTourismApp = () => {
     return (
@@ -11,7 +12,8 @@ const SpaceTourismApp = () => {
             <Routes>
                 {/* <Route path="/" element={<Home/>}/> */}
                 {/* <Route path="/" element={<Destination/>}/> */}
-                <Route path="/" element={<Crew/>}/>
+                {/* <Route path="/" element={<Crew/>}/> */}
+                <Route path="/" element={<Technology/>}/>
             </Routes>
         </>
 
---
+++ src/components/technology/Technology.tsx
@@ -0,0 +1,8 @@
+import './technology.css'
+
+const Technology: React.FC = () => {
+    return (
+        <main id="technology"></main>
+    )
+}
+export default Technology
