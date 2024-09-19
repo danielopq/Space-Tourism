5cdc4deaf7ba22489fdce0e882357ee2b7edc4cc
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-19 14:32:13 +0100

    NavBar.tsx - fully working

--- src/SpaceTourismApp.tsx
+++ src/SpaceTourismApp.tsx
@@ -8,12 +8,12 @@ import Technology from "./components/technology/Technology";
 const SpaceTourismApp = () => {
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <Routes>
-                {/* <Route path="/" element={<Home/>}/> */}
-                {/* <Route path="/" element={<Destination/>}/> */}
-                {/* <Route path="/" element={<Crew/>}/> */}
-                <Route path="/" element={<Technology/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/destination" element={<Destination />} />
+                <Route path="/crew" element={<Crew />} />
+                <Route path="/technology" element={<Technology />} />
             </Routes>
         </>
 
--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,7 +1,22 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import './navBar.css';
+import { useNavigate } from 'react-router-dom';
 
-const NavBar = () => {
+
+/**
+ * Contains the main navigation bar. It remains visible at all times.
+ * @returns {React.FC} - Navigation bar
+ */
+const NavBar: React.FC = () => {
+
+    interface LinkState {
+        homeLinkState: string;
+        destinationLinkState: string;
+        crewLinkState: string;
+        technologyLinkState: string;
+    }
+
+    const navigate = useNavigate();
 
     //Contains the state of the links. When it is inactive, it displays a fixed bottom border, and the cursor doesn't change on hover.
     const [linkState, setLinkState] = useState({
@@ -11,24 +26,25 @@ const NavBar = () => {
         technologyLinkState: 'navbarSectionLink active',
     });
 
-    const homeLink = useRef<HTMLDivElement>(null);
-    const destinationLink = useRef<HTMLDivElement>(null);
-    const crewLink = useRef<HTMLDivElement>(null);
-    const technologyLink = useRef<HTMLDivElement>(null);
-
     const { homeLinkState, destinationLinkState, crewLinkState, technologyLinkState, } = linkState
 
-
-    const handleClick = (link:string) => {
-        let newState = {
+    /**
+     * Displays the different sections of the website and updates the link state (active or inactive).
+     * @param {string} link - Link name
+     */
+    const handleClick = (link: string): void => {
+        console.log()
+        let newState: LinkState = {
             homeLinkState: 'navbarSectionLink active',
             destinationLinkState: 'navbarSectionLink active',
             crewLinkState: 'navbarSectionLink active',
             technologyLinkState: 'navbarSectionLink active',
         }
-
-        setLinkState({...newState,[`${link}State`]:'navbarSectionLink inactive'});
-        console.table(linkState);
+        //If the link is active displays the corresponding section.
+        if (linkState[`${link}LinkState` as keyof LinkState] != 'navbarSectionLink inactive') {
+            link == "home" ? navigate('/') : navigate(`/${link}`);
+        }
+        setLinkState({ ...newState, [`${link}LinkState`]: 'navbarSectionLink inactive' });
     }
 
     return (
@@ -38,19 +54,19 @@ const NavBar = () => {
                 <div id="navBarline"></div>
             </div>
             <div id="navBarRight">
-                <div ref={homeLink} className={homeLinkState} onClick={() => handleClick("homeLink")}>
+                <div className={homeLinkState} onClick={() => handleClick("home")}>
                     <span className='sectionNumber'>00</span>
                     <span className='sectionName'> HOME</span>
                 </div>
-                <div ref={destinationLink} className={destinationLinkState} onClick={() => handleClick("destinationLink")}>
+                <div className={destinationLinkState} onClick={() => handleClick("destination")}>
                     <span className='sectionNumber'>01</span>
                     <span className='sectionName'> DESTINATION</span>
                 </div>
-                <div ref={crewLink} className={crewLinkState} onClick={() => handleClick("crewLink")}>
+                <div className={crewLinkState} onClick={() => handleClick("crew")}>
                     <span className='sectionNumber'>02</span>
                     <span className='sectionName'> CREW</span>
                 </div>
-                <div ref={technologyLink} className={technologyLinkState} onClick={() => handleClick("technologyLink")}>
+                <div className={technologyLinkState} onClick={() => handleClick("technology")}>
                     <span className='sectionNumber'>03</span>
                     <span className='sectionName'> TECHNOLOGY</span>
                 </div>
