70465091321c5b38e831f8056ff09c618b7c3943
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-19 02:55:37 +0100

    NavBar.tsx  - general layout

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,9 +1,18 @@
 import './navBar.css';
 
-const NavBar = () =>{
-    return(
+const NavBar = () => {
+    return (
         <nav>
-        <div id="logo"></div>
+            <div id="navBarLeft">
+                <div id="logo"></div>
+                <div id="navBarline"></div>
+            </div>
+            <div id="navBarRight">
+                <div className='navbarSectionLink inactive'><span className='sectionNumber'>00</span><span className='sectionName'> HOME</span></div>
+                <div className='navbarSectionLink active'><span className='sectionNumber'>01</span><span className='sectionName'> DESTINATION</span></div>
+                <div className='navbarSectionLink active'><span className='sectionNumber'>02</span><span className='sectionName'> CREW</span></div>
+                <div className='navbarSectionLink active'><span className='sectionNumber'>03</span><span className='sectionName'> TECHNOLOGY</span></div>
+            </div>
         </nav>
     )
 }
