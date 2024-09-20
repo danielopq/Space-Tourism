7faeeb1c364f3fb660bdbe4135a33cca2dd5ad04
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-20 02:51:38 +0100

    Home.tsx - finished

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,17 +1,21 @@
 import './home.css'
-const Home = () => {
+
+//Home page
+
+const Home:React.FC = () => {
     return (
         <main id="home">
             <div id="homeMainContent">
                 <div id="homeText">
                     <p id="homeTextHeader">SO, YOU WANT TO TRAVEL TO</p>
                     <h1>SPACE</h1>
-                    <p id="homeTextMain" className='mainText'>Let’s face it; if you want to go to space, you might as well genuinely go to
-                        outer space and not hover kind of on the edge of it. Well sit back, and relax
-                        because we’ll give you a truly out of this world experience!
+                    <p id="homeTextMain" className='mainText'>Let’s face it; if you want to go to space,
+                         you might as well genuinely go to outer space and not hover kind of on the edge
+                          of it. Well sit back, and relax because we’ll give you a truly out of this 
+                          world experience!
                     </p>
                 </div>
-                <div id="homeExploreBt"></div>
+                <button id="homeExploreBt">EXPLORE</button>
             </div>
         </main>
     )
