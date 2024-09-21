6ad34be7b17bcbc9268205dc2a3bad5f223fd36c
danielopq <danielopq@gmail.com>
2024-09-21 17:20:24 +0100

    Destination.tsx - destinationContent general layout

--- src/components/destination/Destination.tsx
+++ src/components/destination/Destination.tsx
@@ -8,7 +8,27 @@ const Destination: React.FC = () => {
                 <span className='sectionTitle'>PICK YOUR DESTINATION</span>
             </div>
             <div id="destinationMain">
-
+                <div id="destinationPic"></div>
+                <div id="destinationContent">
+                    <nav id="destinationNavbar"></nav>
+                    <h1 id="destinationPlanetName">Moon</h1>
+                    <p id="destinationMainText">
+                        See our planet as you’ve never seen it before. A perfect relaxing
+                        trip away to help regain perspective and come back refreshed. While
+                        you’re there, take in some history by visiting the Luna 2 and Apollo
+                        11 landing sites.
+                    </p>
+                    <div id="destinationData">
+                        <div id="destinationDistance">
+                            <div>AVG. DISTANCE</div>
+                            <div>384,400 km</div>
+                        </div>
+                        <div id="destinationTime">
+                            <div>Est. travel time</div>
+                            <div>3 days</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
