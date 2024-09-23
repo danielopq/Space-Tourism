f02e7d043d67ffb765deba8e36f16b815e460c62
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-24 00:33:50 +0100

    pic fade-in and out experiment

--- src/components/destination/Destination.tsx
+++ src/components/destination/Destination.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import DestinationNavbar from './destinationNavBar/DestinationNavBar';
 import destinationData from './destinationData/destInationData';
 import './destination.css'
@@ -12,9 +12,28 @@ const Destination: React.FC = () => {
         travelTime: string,
         picPath: string
     }
-    
+
     const [tripPlan, setTriplan] = useState<TripPlan>(destinationData.moon);
     const { planetName, tripDescription, distance, travelTime, picPath } = tripPlan;
+
+    const destinationPic = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (destinationPic.current) {
+            destinationPic.current.classList.remove('fade-in');
+            destinationPic.current.classList.add('fade-out');
+        }
+        const timeout = setTimeout(() => {
+            if (destinationPic.current) {
+                if (destinationPic.current) {
+                    destinationPic.current.setAttribute('style',`background-image: url(${picPath});`);
+                    destinationPic.current.classList.add('fade-in');
+                }
+            }
+        }, 100);
+
+        return () => clearTimeout(timeout); // Limpia el timeout cuando el componente se desmonte
+    }, [tripPlan]);
 
     return (
         <div id="destination" className='sectionFrame'>
@@ -23,9 +42,9 @@ const Destination: React.FC = () => {
                 <span className='sectionTitle'>PICK YOUR DESTINATION</span>
             </div>
             <div id="destinationMain">
-                <div id="destinationPic" style={{ backgroundImage: `url(${picPath})` }}></div>
+                <div id="destinationPic" ref={destinationPic} className=""></div>
                 <div id="destinationContent">
-                    <DestinationNavbar setTriplan={setTriplan}/>
+                    <DestinationNavbar setTriplan={setTriplan} />
                     <h1 id="destinationPlanetName">{planetName}</h1>
                     <p id="destinationMainText" className='mainText'>{tripDescription}</p>
                     <div id="destinationData">
