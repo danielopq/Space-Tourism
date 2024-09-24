cfb48bc950c028fa6e99c315a9b385798eaa1c27
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-24 14:26:52 +0100

    CrewNavBar.tsx - buttons state done

--- src/components/crew/crewNavbar/CrewNavBar.tsx
+++ src/components/crew/crewNavbar/CrewNavBar.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
 import './crewNavBar.css';
 
-const CrewNavBar:React.FC = ()=>{
-    return(
+interface ButtonState {
+    [buttonRef: string]: string;
+}
+
+const CrewNavBar: React.FC = () => {
+    const [buttonsState, setButtonsState] = useState<ButtonState>({
+        DouglasHurleyBt: 'crewNavBarButton crewNavBarButtonInactive',
+        MarkShuttleworthBt: 'crewNavBarButton crewNavBarButtonActive',
+        VictorGloverBt: 'crewNavBarButton crewNavBarButtonActive',
+        AnoushehAnsariBt: 'crewNavBarButton crewNavBarButtonActive',
+    });
+
+    const { DouglasHurleyBt, MarkShuttleworthBt, VictorGloverBt, AnoushehAnsariBt, } = buttonsState;
+
+    const handleButton = (crewMember: string) => {
+        let newButtonState = {
+            DouglasHurleyBt: 'crewNavBarButton crewNavBarButtonActive',
+            MarkShuttleworthBt: 'crewNavBarButton crewNavBarButtonActive',
+            VictorGloverBt: 'crewNavBarButton crewNavBarButtonActive',
+            AnoushehAnsariBt: 'crewNavBarButton crewNavBarButtonActive',
+        }
+
+        setButtonsState({...newButtonState,[`${crewMember}Bt`]:'crewNavBarButton crewNavBarButtonInactive'})
+    }
+
+    return (
         <nav id="crewNavbar">
-            <div className='crewNavBarButton crewNavBarButtonInactive'></div>
-            <div className='crewNavBarButton crewNavBarButtonActive'></div>
-            <div className='crewNavBarButton crewNavBarButtonActive'></div>
-            <div className='crewNavBarButton crewNavBarButtonActive'></div>
+            <div className={DouglasHurleyBt} onClick={() => handleButton('DouglasHurley')}></div>
+            <div className={MarkShuttleworthBt} onClick={() => handleButton('MarkShuttleworth')}></div>
+            <div className={VictorGloverBt} onClick={() => handleButton('VictorGlover')}></div>
+            <div className={AnoushehAnsariBt} onClick={() => handleButton('AnoushehAnsari')}></div>
         </nav>
     )
 }
