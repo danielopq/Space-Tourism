fbb59f3057550243d7c5b2430bcd011b33d9f087
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-25 00:24:29 +0100

    Technology.tsx - texts format done

--- src/components/technology/Technology.tsx
+++ src/components/technology/Technology.tsx
@@ -11,11 +11,12 @@ const Technology: React.FC = () => {
                 <div className='technologyMainText'>
                     <p className='technologyHeader'>THE TERMINOLOGY...</p>
                     <p className='technologyTerm'>LAUNCH VEHICLE</p>
-                    <p className='technologyDescription'>
-                    A space capsule is an often-crewed spacecraft that uses a blunt-body
-                     reentry capsule to reenter the Earth's atmosphere without wings. 
-                     Our capsule is where you'll spend your time during the flight. 
-                     It includes a space gym, cinema, and plenty of other activities to keep you entertained.
+                    <p className='technologyDescription mainText'>
+                        A launch vehicle or carrier rocket is a rocket-propelled
+                        vehicle used to carry a payload from Earth's surface to
+                        space, usually to Earth orbit or beyond. Our WEB-X carrier
+                        rocket is the most powerful in operation. Standing 150 metres
+                        tall, it's quite an awe-inspiring sight on the launch pad!
                     </p>
                 </div>
                 <div className='technologyPic'></div>
