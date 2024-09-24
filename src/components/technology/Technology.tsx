623011d256445efe9b4c0de166f43ec7b356d926
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-25 00:13:41 +0100

    Technology.tsx - general layout

--- src/components/technology/Technology.tsx
+++ src/components/technology/Technology.tsx
@@ -8,7 +8,17 @@ const Technology: React.FC = () => {
                 <span className='sectionTitle'>SPACE LAUNCH 101</span>
             </div>
             <div id="technologyMain">
-
+                <div className='technologyMainText'>
+                    <p className='technologyHeader'>THE TERMINOLOGY...</p>
+                    <p className='technologyTerm'>LAUNCH VEHICLE</p>
+                    <p className='technologyDescription'>
+                    A space capsule is an often-crewed spacecraft that uses a blunt-body
+                     reentry capsule to reenter the Earth's atmosphere without wings. 
+                     Our capsule is where you'll spend your time during the flight. 
+                     It includes a space gym, cinema, and plenty of other activities to keep you entertained.
+                    </p>
+                </div>
+                <div className='technologyPic'></div>
             </div>
         </div>
     )
