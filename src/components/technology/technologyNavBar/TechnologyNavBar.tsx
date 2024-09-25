9e5ec3befcc84f6354be394b75e84ef503044437
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-25 02:35:05 +0100

    TechnologyNavBar.tsx - buttons states set up

--- src/components/technology/technologyNavBar/TechnologyNavBar.tsx
+++ src/components/technology/technologyNavBar/TechnologyNavBar.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import './TechnologyNavBar.css';
 
-const TechnologyNavBar:React.FC = ()=>{
-    return(
+const TechnologyNavBar: React.FC = () => {
+
+    interface ButtonsState {
+        launchVehicleState: string;
+        spacePortState: string;
+        spaceCapsuleState: string;
+    }
+
+    const [buttonsState, setButtonsState] = useState<ButtonsState>({
+        launchVehicleState: 'techNavBarBt techNavBarBt-active',
+        spacePortState: 'techNavBarBt techNavBarBt-inactive',
+        spaceCapsuleState: 'techNavBarBt techNavBarBt-inactive',
+    });
+    const { launchVehicleState, spacePortState, spaceCapsuleState } = buttonsState;
+
+    const handleClick = (itemName: string):void => {
+        let newButtonsState:ButtonsState={
+            launchVehicleState: 'techNavBarBt techNavBarBt-inactive',
+            spacePortState: 'techNavBarBt techNavBarBt-inactive',
+            spaceCapsuleState: 'techNavBarBt techNavBarBt-inactive',
+        }
+
+        setButtonsState({...newButtonsState,[`${itemName}State`]:'techNavBarBt techNavBarBt-active'});
+    }
+
+    return (
         <nav id="technologyNavBar">
-            <div className='techNavBarBt techNavBarBt-active'>1</div>
-            <div className='techNavBarBt techNavBarBt-inactive'>2</div>
-            <div className='techNavBarBt techNavBarBt-inactive'>3</div>
+            <div className={launchVehicleState} onClick={() => handleClick('launchVehicle')}>1</div>
+            <div className={spacePortState} onClick={() => handleClick('spacePort')}>2</div>
+            <div className={spaceCapsuleState} onClick={() => handleClick('spaceCapsule')}>3</div>
         </nav>
     )
 }
