170405382544f85514ffc8d6dcd6caafdb8ad87e
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-27 11:46:04 +0100

    TerminologyCard.tsx - picture responsize sizes fixed.

--- src/components/technology/technologyData/technologyData.ts
+++ src/components/technology/technologyData/technologyData.ts
@@ -1,11 +1,16 @@
-import spaceVehicle from '../img/image-launch-vehicle-portrait.jpg';
-import spacePortPic from '../img/image-spaceport-portrait.jpg';
-import spaceCapsule from '../img/image-space-capsule-portrait.jpg';
+import spaceVehiclePortrait from '../img/image-launch-vehicle-portrait.jpg';
+import spacePortPortrait from '../img/image-spaceport-portrait.jpg';
+import spaceCapsulePortrait from '../img/image-space-capsule-portrait.jpg';
+
+import spaceVehicleLandscape from '../img/image-launch-vehicle-landscape.jpg';
+import spacePortLandscape from '../img/image-spaceport-landscape.jpg';
+import spaceCapsuleLandscape from '../img/image-space-capsule-landscape.jpg';
 
 interface Item{
     itemName: string,
     description:string,
-    picPath:string,
+    portraitPicPath:string,
+    landScapePicPath:string,
 }
 
 interface ItemsList{
@@ -16,17 +21,20 @@ const technologyData:ItemsList = {
     launchVehicle:{
         itemName: 'LAUNCH VEHICLE',
         description:'A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth\'s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it\'s quite an awe-inspiring sight on the launch pad!',
-        picPath:spaceVehicle,
+        portraitPicPath:spaceVehiclePortrait,
+        landScapePicPath:spaceVehicleLandscape,
     },
     spacePort:{
         itemName: 'SPACEPORT',
         description:'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
-        picPath:spacePortPic,
+        portraitPicPath:spacePortPortrait,
+        landScapePicPath:spacePortLandscape,
     },
     spaceCapsule:{
         itemName: 'SPACE CAPSULE',
         description:'A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth\'s atmosphere without wings. Our capsule is where you\'ll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.',
-        picPath:spaceCapsule,
+        portraitPicPath:spaceCapsulePortrait,
+        landScapePicPath:spaceCapsuleLandscape,
     },
 
 }
--- src/components/technology/terminologyCard/TerminologyCard.tsx
+++ src/components/technology/terminologyCard/TerminologyCard.tsx
@@ -4,7 +4,8 @@ import './terminologyCard.css';
 interface Item{
     itemName:string,
     description:string,
-    picPath:string,
+    portraitPicPath:string,
+    landScapePicPath:string,
 }
 
 interface TerminologyCardProps{
@@ -13,10 +14,33 @@ interface TerminologyCardProps{
     index:number;
 }
 
+/**
+ * Component that contains a terminology card.
+ * @param param0 - item: terminology item to be displayed; active: indicates whether it should be displayed or not; index: z-index of the card.
+ * @returns - Terminology card component. 
+ */
 const TerminologyCard: React.FC<TerminologyCardProps> = ({item,active,index}) => {
-    const {itemName,description,picPath,} = item;
-    const [cardOpacity,setCardOpacity] = useState<number>(0);
+    const {itemName,description,portraitPicPath,landScapePicPath} = item;
+    const [cardOpacity,setCardOpacity] = useState<number>(0); //component opacity
+    let windowWidth: number = window.innerWidth; //window width
 
+    const [cardPicture,setCardpicture] = useState<string>(portraitPicPath); //Controls which picture size is displayed depending on the window size.
+
+    /**
+     * Sets up the picture size depending on the window size.
+     */
+    const setUpPictureSize = ():void=>{
+        windowWidth = window.innerWidth;
+        (windowWidth >= 768 && windowWidth <= 1024 ) ? setCardpicture(landScapePicPath) : setCardpicture(portraitPicPath);
+    }
+
+    //Sets up the initial picture size and adds and event listener to the window.
+    useEffect(()=>{
+        setUpPictureSize();
+        window.addEventListener('resize',setUpPictureSize);
+    },[])
+
+    //Sets the component's opacity depending on the 'active' state.
     useEffect(()=>{
         active ? setCardOpacity(1) : setCardOpacity(0);
     },[active]);
@@ -28,7 +52,7 @@ const TerminologyCard: React.FC<TerminologyCardProps> = ({item,active,index}) =>
                 <p className='technologyItem'>{itemName}</p>
                 <p className='technologyDescription mainText'>{description}</p>
             </div>
-            <div className='technologyPic' style={{ backgroundImage: `url(${picPath})` }}></div>
+            <div className='technologyPic' style={{ backgroundImage: `url(${cardPicture})` }}></div>
         </div>
     )
 }
