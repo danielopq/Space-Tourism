b6fe3db91c93787c07210ac7e2deaf526f56c922
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-28 12:58:13 +0100

    backgrounds pics manager fixed

--- src/SpaceTourismApp.tsx
+++ src/SpaceTourismApp.tsx
@@ -1,36 +1,65 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import '../src/assets/css/SpaceTourismApp.css';
 import NavBar from './components/navBar/NavBar';
 import Home from "./components/home/Home";
 import Destination from "./components/destination/Destination";
 import Crew from "./components/crew/Crew";
 import Technology from "./components/technology/Technology";
-import { useState } from "react";
+
+import * as backGroundPics from '../src/assets/img';
 
 /**
  * Main component containing all sections and their respective routes.
  * @returns {React.FC} Main component
  */
+
+type BackgroundPicKeys = 'homeDesktop' | 'homeMobile' | 'homeTablet' | 'destinationDesktop' | 'destinationMobile' | 'destinationTablet' | 'crewDesktop' | 'crewMobile' | 'crewTablet' | 'technologyDesktop' | 'technologyMobile' | 'technologyTablet';
+
 const SpaceTourismApp: React.FC = () => {
 
-    const [backGroundPic, setBackGroundPic] = useState<string>("homeBackGround");
     const [currentSection, setCurrentSection] = useState<string>('home');
+    const [backgroundPicture,setBackgroundPicture] = useState<BackgroundPicKeys>('homeDesktop');
+    let windowWidth: number = window.innerWidth; //window width
+
+    
+    /**
+    * Sets up the picture size depending on the window size.
+    */
+    const setWindowWidth = ():void => {
+        windowWidth = window.innerWidth;
+        selectPicture(currentSection);
+        console.log(currentSection);
+    }
+
+    const selectPicture = (section:string):void => {
+        let backgroundPicture:BackgroundPicKeys = 'homeDesktop';
+        windowWidth < 768 && (backgroundPicture = section+"Mobile");
+        (windowWidth >= 768 && windowWidth <= 1024 ) && (backgroundPicture = section+"Tablet");
+        windowWidth > 1024 && (backgroundPicture = section+"Desktop");
+        setBackgroundPicture(backgroundPicture as BackgroundPicKeys);
+    }
+
+    //Sets up the initial picture size and adds and event listener to the window.
+    useEffect(() => {
+        window.addEventListener('resize', setWindowWidth);
+    }, [])
 
     /**
      * Changes the background image and switches the current section to the desired one.
      * @param {string} section - Selected section to be desplayed.
      */
     const changeSection = (section: string): void => {
-        setBackGroundPic(`${section}BackGround`);
         setCurrentSection(section);
+        selectPicture(section);
     }
 
     return (
-        <main className={backGroundPic}>
-            <NavBar currentSection={currentSection}/>
+        <main style={{ backgroundImage: `url(${backGroundPics[backgroundPicture as keyof typeof backGroundPics]})` }}>
+            <NavBar currentSection={currentSection} />
             <Routes>
-                <Route path="/" element={<Home changeSection={changeSection}/>} />
-                <Route path="/destination" element={<Destination changeSection={changeSection}  />} />
+                <Route path="/" element={<Home changeSection={changeSection} />} />
+                <Route path="/destination" element={<Destination changeSection={changeSection} />} />
                 <Route path="/crew" element={<Crew changeSection={changeSection} />} />
                 <Route path="/technology" element={<Technology changeSection={changeSection} />} />
             </Routes>
---
+++ src/assets/img/index.ts
@@ -0,0 +1,15 @@
+export { default as homeDesktop } from './home/background-home-desktop.jpg';
+export { default as homeTablet } from './home/background-home-tablet.jpg';
+export { default as homeMobile } from './home/background-home-mobile.jpg';
+
+export { default as destinationDesktop } from './destination/background-destination-desktop.jpg';
+export { default as destinationTablet } from './destination/background-destination-tablet.jpg';
+export { default as destinationMobile } from './destination/background-destination-mobile.jpg';
+
+export { default as crewDesktop } from './crew/background-crew-desktop.jpg';
+export { default as crewTablet } from './crew/background-crew-tablet.jpg';
+export { default as crewMobile } from './crew/background-crew-mobile.jpg';
+
+export { default as technologyDesktop } from './technology/background-technology-desktop.jpg';
+export { default as technologyTablet } from './technology/background-technology-tablet.jpg';
+export { default as technologyMobile } from './technology/background-technology-mobile.jpg';
